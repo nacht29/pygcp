649cecb42daf848dba8ceba33e2304bd10fb4092
nacht29 <nacht29.studygmail.com>
2025-04-02 23:13:39 +0800

    feat: added bash script to launch versions

---
+++ launch.sh
@@ -0,0 +1,5 @@
+rm -r dist/
+rm -r src/pygcp.egg-info/
+python3 -m build
+python3 -m twine upload --repository testpypi dist/*
+# python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps pygcp==1.0.2 --break-system- --force-reinstall
